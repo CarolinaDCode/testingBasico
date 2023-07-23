1e8e596f7e38390bd16ed45e087a4b5b25c0aaf0
diana <diana.carhuamanta@deister.es>
2023-07-23 16:13:46 -0500

    creacion de consultas para crear y eliminar email, datos de la bases de datos

--- src/main/java/academy/atl/newsletters/repository/EmailRepository.java
+++ src/main/java/academy/atl/newsletters/repository/EmailRepository.java
@@ -6,17 +6,37 @@ import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class EmailRepositoty {
+public class EmailRepository {
 
     @PersistenceContext
-    EntityManager baseDeDatos;
+    private EntityManager baseDeDatos;
 
     @Transactional
-    public void guardarEmail(String email){
+    public void guardarEmail(String emailDelUsuario){
+        /**
+         * Consulta HQL: Hibernate ...
+         * Evitando el Hacking de Inyección SQL
+         */
+        String consultaSql = "INSERT INTO lead (email) VALUES (:paramEmail)";
+        baseDeDatos.createQuery(consultaSql)
+                   .setParameter("paramEmail", emailDelUsuario)
+                   .executeUpdate();
+
+        /**
+         * Consulta SQL
+         */
+        /**
         String consultaSql = "INSERT INTO `lead` (`email`)" +
                 "VALUES ('"+ email +"')";
-        baseDeDatos.createNativeQuery(consultaSql);
+        baseDeDatos.createNativeQuery(consultaSql).executeUpdate();
+         */
     }
 
-
+    @Transactional
+    public void eliminarEmail(String emailDelUsuario) {
+        String consultaSql = "DELETE FROM lead WHERE email = :paramEmail";
+        baseDeDatos.createQuery(consultaSql)
+                   .setParameter("paramEmail", emailDelUsuario)
+                   .executeUpdate();
+    }
 }
